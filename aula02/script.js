c068c75dc41eb433d6e4c7c30f9dc07faae962b6
Rafaela do Nascimento Guimarães <guimaraesrafaela566@gmail.com>
2026-03-17 10:22:48 -0300

    Commit inicial

---
+++ aula02/script.js
@@ -0,0 +1,31 @@
+function validar(){
+    valor = document.getElementById("txtValor").value
+
+    var divResult = document.getElementById("divResult")
+
+    if( isNaN(valor)){
+        divResult.innerHTML ="O valor digitado não é um número"
+
+    }else if(valor < 1 || valor > 10){
+        divResult.innerHTML = "O valor não é permitido"
+    }else{
+        divResult.innerHTML = "Valor permitido"
+    }
+}
+
+function limpar(){
+    divResult = document.getElementById("divResult").innerHTML = ""
+}
+
+$("#divJquery").css("background", "#000")
+$("#divJquery").css("color", "white")
+$("#divJquery").html("SIX SEVEN <hr> Meia SEVEN")
+
+
+$("#butao").on("click", function(){
+    $("#divJquery").toggle(2000, mostrarMensagem)
+})
+
+function mostrarMensagem(){
+    alert("Ação finalizada")
+}
