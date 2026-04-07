6d245ce22f1a489c3a93a50822b72566ae668112
Rafaela do Nascimento Guimarães <guimaraesrafaela566@gmail.com>
2026-04-07 12:03:09 -0300

    Aula 6 retornando o JSON

--- aula05_json/script.js
+++ aula05_json/script.js
@@ -33,7 +33,7 @@ function read_products(){
             var products = obj_json.produtos;
             var txt = "";
 
-            if(products,length == 0){
+            if(products.length == 0){
                 txt ="<tr><th> Nenhum produto </th></tr>";
             }else{
 
@@ -41,6 +41,7 @@ function read_products(){
                 txt += " <th> Código  </th>";
                 txt += " <th> Nome  </th>";
                 txt += " <th> Preço  </th>";
+                txt += " <th> Deletar  </th>";
                 txt += "<tr>";
 
                 products.forEach(product =>{
@@ -48,6 +49,7 @@ function read_products(){
                     txt += " <td>" + product.id + "</td>";
                     txt += " <td>" + product.nome + "</td>";
                     txt += " <td>" + product.preco + "</td>";
+                    txt += " <td> <button onclick='deletar(" + product.id + ")'> X </button></td>";
                     txt += "<tr>";
                 })
             }
@@ -57,4 +59,51 @@ function read_products(){
 
     req.open("GET" , "servidor.php?buscar" , true);
     req.send();
+}
+
+function deletar(id_produto){
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var objson = JSON.parse(this.responseText);
+            if(objson.resposta){
+                alert("Produto excluído com sucesso")
+                read_products();
+            }
+        }
+    };
+
+    req.open( "GET", "servidor.php?excluir&idProduto=" + id_produto, true);
+    req.send()
+}
+
+
+function add(id_produto){
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+
+            var objson = JSON.parse(this.responseText);
+
+            if(objson.resposta){
+                if(objson.id){
+                    alert(objson.resposta + "\nID:" + objson.id);
+                    read_products();
+                }else
+                    alert(objson.resposta);
+                
+            }
+        }
+    };
+
+    var nome = document.getElementById("txt_nome").value;
+    var preco = document.getElementById("txt_preco").value;
+    if(nome != ""){
+        document.getElementById("txt_nome").value = "";
+        document.getElementById("txt_preco").value = "";
+
+        req.open( "POST", "servidor.php?inserir", true);
+        req.setRequestHeader("Content-type" , "application/x-www-form-urlencoded")
+        req.send("name=" + nome + "&price=" + preco)
+    }
 }
